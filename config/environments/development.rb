8b428b0fe8682e174743789200acc82148568f03
Efrain AWS <efrados@gmail.com>
2021-04-14 15:19:53 +0000

    Add account validation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts.clear
+  config.action_mailer.raise_delivery_errors = false
+  host = '07b60a76f0794d7e88cd5d4d225e0342.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
